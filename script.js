df1a95965532872fa04d1b0d2fe16362987e7752
kapilexior <116294539+kapilexior@users.noreply.github.com>
2024-04-11 22:58:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,17 +7,17 @@
  }
 
  function z38() {
-    window.open("z38_Pol.html", "okienko_z38","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+    window.open("z_38_Pol.html", "okienko_z38","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
  }
  
  function z39() {
-    window.open("z39_Pol.html", "okienko_z39","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+    window.open("z_39_Pol.html", "okienko_z39","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
  }
 
  function z40() {
-    window.open("z40_Pol.html", "okienko_z40","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+    window.open("z_40_Pol.html", "okienko_z40","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
  }
 
  function z38w() {
-    window.open("z38w_Pol.html", "okienko_z38","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+    window.open("z_38w_Pol.html", "okienko_z38","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
  }
