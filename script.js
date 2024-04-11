1a0fd5e3e91edd66148738b3f14d7044c8a0fc2d
kapilexior <116294539+kapilexior@users.noreply.github.com>
2024-04-11 22:32:10 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+ function z35() {
+    window.open("z35_Pol.html", "okienko_z35","toolbar=no, directories=no, menubar=no, height=800, width=900, top=350, left=600");
+ }
+ 
+ function z36() {
+    window.open("z36_Pol.html", "okienko_z36","toolbar=no, directories=no, menubar=no, height=800, width=900, top=350, left=600");
+ }
+
+ function z38() {
+    window.open("z38_Pol.html", "okienko_z38","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+ 
+ function z39() {
+    window.open("z39_Pol.html", "okienko_z39","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function z40() {
+    window.open("z40_Pol.html", "okienko_z40","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function z38w() {
+    window.open("z38w_Pol.html", "okienko_z38","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
